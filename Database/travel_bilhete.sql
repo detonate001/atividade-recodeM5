c965a4fd447280953ad9547a367540a30f48c412
detonate001 <gabriel_leao97@hotmail.com>
2022-10-24 22:54:07 -0300

    Adicionando DB, MDC e MDL e update no README

---
+++ Database/travel_bilhete.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: travel
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bilhete`
+--
+
+DROP TABLE IF EXISTS `bilhete`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bilhete` (
+  `id_bilhete` int NOT NULL AUTO_INCREMENT,
+  `data_bilhete` date DEFAULT NULL,
+  `promocao` int DEFAULT NULL,
+  `hora` int DEFAULT NULL,
+  `fk_id_cliente` int DEFAULT NULL,
+  PRIMARY KEY (`id_bilhete`),
+  KEY `fk_id_cliente` (`fk_id_cliente`),
+  CONSTRAINT `bilhete_ibfk_1` FOREIGN KEY (`fk_id_cliente`) REFERENCES `cliente` (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bilhete`
+--
+
+LOCK TABLES `bilhete` WRITE;
+/*!40000 ALTER TABLE `bilhete` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bilhete` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-12 19:22:49
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# atividade-recodeM5
+
+# Recode 
+
+Atividade para o curso Recode (React, framework Spring Data, java )
+
+
+
+## Conteudo
+
+ - CRUD (JAVA)
+ - Modelo conceitual do banco de dados (BRModelo)
+ - Banco de dados (SQL)
+ - React
+
+## 🚀 Aluno
+ - Gabriel Leão.
+
